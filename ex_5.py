850f56c640700c7bdb869e7922cdea0c30f81126
Humberto <humbertogabriel.nit@gmail.com>
2026-05-31 15:04:42 -0300

    Limpeza: removendo arquivos duplicados da raiz

--- ex_5.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Digite um número qualquer: '))
-lista_primos = []
-for i in range(1,num):
-    if i % 2 != 0:
-        lista_primos.append(i)  
-print(f'Lista de números primos entre 1 e {num} = {lista_primos} ')
-
